c6d452f59c396561ab116d3145a0d34f85fb03c5
john <johnwcb@gmail.com>
2020-03-30 18:50:39 -0700

    77: recent updates

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-const HatsPage = (props) => {
-    console.log('hhh', props)
+function App() {
     return (
         <div>
-            <h1>HAT PAGE</h1>
-        </div>
-    )
-};
-function App(props) {
-    console.log('aapp', props)
-    return (
-        <div>
+            <Header />
             <Switch>
                 <Route exact path='/' component={HomePage} />
-                <Route path='/hats' component={HatsPage} />
+                <Route path='/shop' component={ShopPage} />
+                <Route path='/signin' component={SignInAndSignUp} />
             </Switch>
         </div>
     );
