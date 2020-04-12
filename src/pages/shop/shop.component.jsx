403682a854051d63d1f37546c63c6e1f75446ecb
john <johnwcb@gmail.com>
2020-04-12 11:21:34 -0700

    130: added more state to Redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-    render() {
-        return (
-            <div className="shop-page">
-                {
-                    this.state.collections.map(({ id, ...otherCollectionProps }) => (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-        );
-    }
-}
+const ShopPage = () => (
+    < div className="shop-page" >
+        <CollectionsOverview />
+    </div >
+);
 
 export default ShopPage;
