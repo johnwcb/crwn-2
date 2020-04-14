0fb493d2a0d87a6a9f573e7efc437b4163b25acf
john <johnwcb@gmail.com>
2020-04-14 12:18:27 -0700

    137: from 131 to 137

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,4 +1,4 @@
-import {createSelector} from 'reselect';
+import { createSelector } from 'reselect';
 
 const selectShop = state => state.shop;
 
@@ -6,3 +6,14 @@ export const selectShopCollections = createSelector(
     [selectShop],
     shop => shop.collections
 );
+
+export const selectCollectionsForPreview = createSelector(
+    [selectShopCollections],
+    collections => Object.keys(collections).map(key=>collections[key])
+);
+
+export const selectCollection = collectionUrlParam =>
+    createSelector(
+        [selectShopCollections],
+        collections => collections[collectionUrlParam]
+    );
